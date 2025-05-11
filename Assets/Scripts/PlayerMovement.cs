3ea1bcdb9d134821c0c7b4a3610ab5e07c122339
Melina Morales <melina.morales@mi.unc.edu.ar>
2025-05-11 18:54:34 -0300

    jump implementation
    If "Jump" button is pressed more time, will jump higher

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,38 +2,84 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody2D playerBody;
+    public Rigidbody2D rb;
     public SpriteRenderer sprite;
     public int speed = 5;
+   
     private bool isFacingRight;
     private Animator animator;
+    private float moveInput;
+    
+    [Header("Jump")]
+    public Transform feetPos;
+    public float checkRadius;
+    public float jumpForce = 5;
+    public LayerMask whatIsGround;
+    private float jumpCoolDown;
+    private bool isJumping;
+    [SerializeField] private float jumpTime = 0.25f;
+    [SerializeField] private bool isGrounded;
+
+    [Header("Fall Physics")]
+    public float fallMultiplier;
+    public float lowJumpMultiplier;
 
     void Start()
     {
         isFacingRight = true;
-        animator = GetComponent<Animator>();
+        isGrounded = true;
+        animator = GetComponent<Animator>();   
     }
 
     void Update()
     {
-        float horizontalAxis = Input.GetAxis("Horizontal");
-        
-        FlipSprite(horizontalAxis);
+        // jump logic
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            playerBody.linearVelocity = Vector2.left * speed;
-            animator.SetFloat("xVelocity", -playerBody.linearVelocity.x);
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            playerBody.linearVelocity = Vector2.right * speed;
-            animator.SetFloat("xVelocity", playerBody.linearVelocity.x);
+        if(isGrounded && Input.GetButtonDown("Jump")) {
+            rb.linearVelocity = Vector2.up * jumpForce;
+            isJumping = true;
+            jumpCoolDown = jumpTime;
         }
 
-        if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow)) {
-            animator.SetFloat("xVelocity", 0f);
+        // while holding jump button -> goes higher
+        if(Input.GetButton("Jump") && isJumping) {
+            if (jumpCoolDown > 0) { 
+                rb.linearVelocity = Vector2.up * jumpForce;
+                jumpCoolDown -= Time.deltaTime;
+            } else
+            {
+                isJumping = false;
+            }
         }
+
+        if (Input.GetButtonUp("Jump")) {
+            isJumping = false;
+        }
+    }
+
+    // Has the frequency of the physics system; it is called every fixed frame-rate frame.
+    void FixedUpdate()
+    {
+        // Horizontal movement
+        moveInput = Input.GetAxisRaw("Horizontal");
+        float horizontalAxisFloat = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(moveInput * speed * Time.fixedDeltaTime, rb.linearVelocity.y);
+        animator.SetFloat("xVelocity", moveInput *  rb.linearVelocity.x);
+
+
+        FlipSprite(horizontalAxisFloat);
+
+        // jump fall physics.
+        if (rb.linearVelocity.y < 0)
+        {
+            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.linearVelocity.y > 0 && Input.GetKey(KeyCode.Space))
+        {
+            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+
     }
 
     private void FlipSprite(float horizontalAxis) {
@@ -43,5 +89,20 @@ public class PlayerMovement : MonoBehaviour
             isFacingRight = !isFacingRight;
         }
     }
+
+    private void Jump() {
+        // Debug.Log("jumping lol");
+        // Vector2.up * jumpForce / cantidad de tiempo de largo de salto
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        jumpCoolDown = Time.time;
+    }
+
+    //private void OnTriggerEnter2D(Collider2D collider) {
+    //    isGrounded = true;
+    //}
+
+    //private void OnTriggerExit2D(Collider2D collider) {
+    //    isGrounded = false;
+    //}
 
 }
