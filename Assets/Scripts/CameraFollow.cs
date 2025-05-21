16c9a6bbf45cafb2bb9dc40a48d4e2b0d8bba32f
Melina Morales <melina.morales@mi.unc.edu.ar>
2025-05-21 08:11:31 -0300

    level implementation

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,10 +6,10 @@ public class CameraFollow : MonoBehaviour
     public float y0ffset = 1f;
     public float x0ffset = 1f;
     public Transform target; // will return the position of the player
-   
+
     void Update()
     {
-       Vector3 newPos = new Vector3(target.position.x + x0ffset, target.position.y + y0ffset, -10f);
+        Vector3 newPos = new Vector3(target.position.x, target.position.y + y0ffset, -10f);
         transform.position = Vector3.Slerp(transform.position, newPos, followSpeed * Time.deltaTime);
     }
 }
